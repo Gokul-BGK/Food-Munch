f3ab74058a11e4936da4939efd9d104d312e828a
Gokul G <g.gokul280703@gmail.com>
2025-02-01 10:35:37 +0530

    Create foodmunch.js

---
+++ foodmunch.js
@@ -0,0 +1,13 @@
+let navbar = document.getElementById("navbar")
+let sectionhome = document.getElementById("sectionhome");
+let sectionnonvegstarters = document.getElementById("sectionnonvegstarters");
+let sectionvegstarters = document.getElementById("sectionvegstarters");
+let sectionsoups = document.getElementById("sectionsoups");
+let sectionfishandseafood = document.getElementById("sectionfishandseafood");
+let sectionmaincourse = document.getElementById("sectionmaincourse");
+let sectionnoodles = document.getElementById("sectionnoodles");
+let sectionsalads = document.getElementById("sectionsalads");
+let sectiondeserts = document.getElementById("sectiondeserts");
+let sectionvideo = document.getElementById("sectionvideo");
+
+localStorage.setItem("navbar", navbar)
